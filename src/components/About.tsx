8bbcd0826d8e0936b79cce325d69bae5841ff4f5
Matt Legowski <googer@pobox.com>
2022-03-31 14:12:48 -0400

    Remove ko-fi link

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -55,15 +55,6 @@ export default function About() {
             marginRight: "auto",
           }}
         >
-          <img
-            height="36"
-            style={{
-              border: "0px",
-              height: "36px",
-            }}
-            src="https://cdn.ko-fi.com/cdn/kofi1.png?v=3"
-            alt="Buy Me a Coffee at ko-fi.com"
-          />
         </a>
         <hr className="separator" />
         <p>Based on</p>
